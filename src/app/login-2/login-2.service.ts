b24ded4c59c69cc4a946142236de07bdc7821da8
Justin916 <gjusincommon@gmail.com>
2020-05-24 05:42:34 +0100

    login change
    login chage

--- src/app/login-2/login-2.service.ts
+++ src/app/login-2/login-2.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from "@angular/core";
 import { ILoginData } from "./interfaces/ILoginData";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: "root",
 })
 export class Login2Service {
-    constructor() {}
+    constructor(private _client: HttpClient) {}
 
     auth1(loginData: ILoginData) {
         console.log(loginData);
+        this._client
+            .get("http://localhost:8080/auth/login-2/Justin1/Password")
+            .subscribe((value) => {
+                console.log(value);
+            });
     }
 }
